b2f5d6fea066e140ac6567dfffbc747338168c41
viin <flavioirish@gmail.com>
2024-02-03 02:32:04 +0000

    feat: add faz login e dashboard

--- actions.py
+++ actions.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 import psycopg2 as psycopg2
 
 
@@ -39,3 +37,83 @@ def criar_usuario(nome, email, senha, idade, telefone, cliente):
     except Exception as e:
         print(e)
 
+
+def buscar_usuario(email):
+    """
+  Função para buscar um usuário no banco de dados pelo seu e-mail.
+
+  Args:
+    email: O e-mail do usuário a ser buscado.
+
+  Returns:
+    Um dicionário com as informações do usuário ou None se não for encontrado.
+  """
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="postgres",
+            user="postgres",
+            password="postgres"
+        )
+        # Crie um cursor
+        cur = conn.cursor()
+
+        cur.execute(f"""
+      SELECT * FROM public.db_clientes WHERE email = '{email}';
+    """)
+
+        # Obtenha o resultado da consulta
+        usuario = cur.fetchone()
+        print(usuario)
+        # Feche o cursor e a conexão
+        cur.close()
+        conn.close()
+
+        if usuario:
+            # Retorna um dicionário com as informações do usuário
+            return {
+                "nome": usuario[1],
+                "email": usuario[2],
+                "senha": usuario[3],
+                "idade": usuario[4],
+                "telefone": usuario[5],
+                "cliente": usuario[6],
+            }
+        else:
+            # Retorna None se o usuário não for encontrado
+            return None
+
+    except Exception as e:
+        print(e)
+        return None
+
+
+def login_user(email, senha):
+    """
+  Função para realizar o login de um usuário no sistema.
+
+  Args:
+    email: O e-mail do usuário.
+    senha: A senha do usuário.
+
+  Returns:
+    Um dicionário com as informações do usuário se o login for bem-sucedido, None caso contrário.
+  """
+    try:
+        usuario = buscar_usuario(email)
+        print(usuario)
+        # Verifica se o usuário foi encontrado
+        if not usuario:
+            return None
+
+        # Verifica se a senha digitada confere com a senha armazenada no banco
+        if usuario["senha"] != senha:
+            return None
+
+        # Retorna as informações do usuário
+        return usuario
+
+    except Exception as e:
+        print(e)
+        return None
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, session
 
-from actions import criar_usuario
+from actions import criar_usuario, login_user
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -20,6 +20,22 @@ def login():
     return render_template('Login.html')
 
 
+@app.route('/fazlogin', methods=['POST'])
+def faz_login():
+    usuario = request.get_json()
+
+    usuario = login_user(usuario['email'], usuario['senha'])
+
+    if usuario is not None:
+        # Login bem-sucedido
+        print(f"Login realizado com sucesso! Bem-vindo(a), {usuario['nome']}")
+        return {'status': 'true', 'usuario': usuario}, 200
+    else:
+        # Login falhou
+        print("Login falhou. E-mail ou senha incorretos.")
+        return {'status': 'false'}, 401
+
+
 @app.route('/register')
 def register():
     return render_template('FormCriar.html')
@@ -28,8 +44,13 @@ def register():
 @app.route('/criar_usuario', methods=['POST'])
 def criar_usuario_api():
     usuario = request.get_json()
-    criar_usuario(usuario['nome'], usuario['email'],  usuario['senha'], usuario['idade'], usuario['telefone'], usuario['tipo'])
-    return {'status': 'success'}, 200
+    try:
+        criar_usuario(usuario['nome'], usuario['email'], usuario['senha'], usuario['idade'], usuario['telefone'],
+                      usuario['tipo'])
+        print("criou user")
+        return {'status': 'true'}, 200
+    except Exception:
+        return {'status': 'false'}, 500
 
 
 @app.route('/categorias/diamond')
@@ -52,10 +73,9 @@ def rent(car_name):
     return render_template('Alugar.html', car_name=car_name)
 
 
-@app.route('/dash/<info_alugar>')
-def dash(info_alugar):
-    return render_template('Dashboard.html', info_alugar=info_alugar)
-
+@app.route('/dash')
+def dash():
+    return render_template('Dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
